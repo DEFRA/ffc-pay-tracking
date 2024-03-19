6d77f837382134535dc5f9b9b9428e962ba5a444
JungaBunga <amir.ishmuhametov@gmail.com>
2024-03-19 13:25:53 +0000

    added-config-for-fetching-report-data

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,9 @@
 const messageConfig = require('./message')
 const databaseConfig = require('./database')
+const serverConfig = require('./server')
 
 module.exports = {
   messageConfig,
-  databaseConfig
+  databaseConfig,
+  serverConfig
 }
