68f21694ed4bfcb3fcbe6e8f2bf9c2e58099ebf8
Sam Plackett <samuel.plackett@eviden.com>
2024-05-17 16:30:01 +0100

    added tests

--- test/unit/app/warning/update.test.js
+++ test/unit/app/warning/update.test.js
@@ -1,32 +1,80 @@
-const { updateWarning } = require('../../../../app/warning/update')
-const { createData } = require('../../../../app/warning/create-data')
-const { getWhereFilter } = require('../../../../app/get-where-filter')
 const db = require('../../../../app/data')
+const { createData } = require('../../../../app/warning/create-data')
+const { BATCH_REJECTED, BATCH_QUARANTINED } = require('../../../../app/constants/warnings')
+const { getWhereFilter } = require('../../../../app/get-where-filter')
+const { updateWarning } = require('../../../../app/warning/update')
 
+jest.mock('../../../../app/data')
 jest.mock('../../../../app/warning/create-data')
 jest.mock('../../../../app/get-where-filter')
-jest.mock('../../../../app/data')
 
-test('updateWarning function updates report data', async () => {
-  const mockEvent = { type: 'test', subject: 'test' }
-  const mockData = { test: 'data' }
-  const mockTransaction = { commit: jest.fn(), rollback: jest.fn() }
-  const mockWhere = { test: 'where' }
+describe('updateWarning', () => {
+  let transaction
 
-  db.sequelize.transaction.mockResolvedValue(mockTransaction)
-  createData.mockResolvedValue(mockData)
-  getWhereFilter.mockReturnValue(mockWhere)
-  db.reportData.update.mockResolvedValue()
-
-  await updateWarning(mockEvent)
-
-  expect(db.sequelize.transaction).toHaveBeenCalled()
-  expect(createData).toHaveBeenCalledWith(mockEvent)
-  expect(db.reportData.update).toHaveBeenCalledWith({ ...mockData }, {
-    where: {
-      daxFileName: mockEvent.subject
+  beforeEach(() => {
+    transaction = {
+      commit: jest.fn(),
+      rollback: jest.fn()
+    }
+    db.sequelize.transaction.mockResolvedValue(transaction)
+    db.reportData = {
+      update: jest.fn()
     }
   })
-  expect(mockTransaction.commit).toHaveBeenCalled()
-  expect(mockTransaction.rollback).not.toHaveBeenCalled()
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should skip update if event type is BATCH_REJECTED', async () => {
+    const event = { type: BATCH_REJECTED }
+
+    await updateWarning(event)
+
+    expect(createData).not.toHaveBeenCalled()
+    expect(db.reportData.update).not.toHaveBeenCalled()
+  })
+
+  test('should skip update if event type is BATCH_QUARANTINED', async () => {
+    const event = { type: BATCH_QUARANTINED }
+
+    await updateWarning(event)
+
+    expect(createData).not.toHaveBeenCalled()
+    expect(db.reportData.update).not.toHaveBeenCalled()
+  })
+
+  test('should update report data with subject', async () => {
+    const event = { type: 'someType', subject: 'subjectFile' }
+    const dbData = { id: 1 }
+    createData.mockResolvedValue(dbData)
+
+    await updateWarning(event)
+
+    expect(createData).toHaveBeenCalledWith(event)
+    expect(db.reportData.update).toHaveBeenCalledWith({ ...dbData }, {
+      where: {
+        daxFileName: 'subjectFile'
+      }
+    })
+    expect(transaction.commit).toHaveBeenCalled()
+  })
+
+  test('should update report data with where filter', async () => {
+    const event = { type: 'someType', data: { someData: 'someValue' } }
+    const dbData = { id: 1 }
+    const where = { someField: 'someValue' }
+    createData.mockResolvedValue(dbData)
+    getWhereFilter.mockReturnValue(where)
+
+    await updateWarning(event)
+
+    expect(createData).toHaveBeenCalledWith(event)
+    expect(getWhereFilter).toHaveBeenCalledWith(event)
+    expect(db.reportData.update).toHaveBeenCalledWith({ ...dbData }, {
+      where,
+      transaction
+    })
+    expect(transaction.commit).toHaveBeenCalled()
+  })
 })
