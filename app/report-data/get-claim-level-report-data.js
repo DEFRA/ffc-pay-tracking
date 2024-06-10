480f6d8bbc32d4e7f0eda90bf327556f2635811c
JungaBunga <amir.ishmuhametov@gmail.com>
2024-06-10 08:49:34 +0100

    changed get-claim-report to correctly provide requested data

--- app/report-data/get-claim-level-report-data.js
+++ app/report-data/get-claim-level-report-data.js
@@ -6,60 +6,49 @@ const getClaimLevelReportData = async (startDate, endDate) => {
   if (startDate && endDate) {
     startDateEndDate = `WHERE "lastUpdated" BETWEEN '${startDate}' AND '${endDate}'`
   }
-  const reportDataIdsResult = await db.sequelize.query(`
-  SELECT rd."reportDataId"
-  FROM "reportData" rd
-  INNER JOIN (
-    SELECT 
-      "sourceSystem", 
-      "frn",
-      CASE
-        WHEN "sourceSystem" = :CS THEN CAST("claimNumber" AS VARCHAR)
-        WHEN "sourceSystem" = :FDMR THEN NULL
-        ELSE CAST("marketingYear" AS VARCHAR)
-      END AS marketingYear,
-      CASE
-        WHEN "sourceSystem" = :BPS THEN NULL
-        WHEN "sourceSystem" = :FDMR THEN NULL
-        WHEN "sourceSystem" = :CS THEN NULL
-        ELSE CAST("agreementNumber" AS VARCHAR)
-      END AS agreementNumber,
-      MAX("paymentRequestNumber") AS maxPaymentRequestNumber
-    FROM "reportData"
-    ${startDateEndDate}
-    GROUP BY 
-      "sourceSystem", 
-      "frn",
-      marketingYear,
-      agreementNumber
-  ) sub
-  ON rd."sourceSystem" = sub."sourceSystem"
-  AND rd."frn" = sub."frn"
-  AND CAST(rd."marketingYear" AS VARCHAR) = sub.marketingYear
-  AND CAST(rd."agreementNumber" AS VARCHAR) = sub.agreementNumber
-  AND rd."paymentRequestNumber" = sub.maxPaymentRequestNumber
+  return db.sequelize.query(`
+  WITH "rankedData" AS (
+    SELECT
+      *,
+      ROW_NUMBER() OVER (
+        PARTITION BY
+          "sourceSystem",
+          frn,
+          CASE 
+              WHEN "sourceSystem" = :BPS THEN NULL
+              WHEN "sourceSystem" = :FDMR THEN NULL
+              WHEN "sourceSystem" = :CS THEN "claimNumber"
+              ELSE "agreementNumber"
+          END,
+          CASE
+            WHEN "sourceSystem" = :FDMR THEN NULL
+            WHEN "sourceSystem" = :CS THEN NULL
+            ELSE "marketingYear"
+          END
+        ORDER BY
+          "paymentRequestNumber" DESC,
+          "lastUpdated" DESC
+      ) AS row_num
+    FROM
+      "reportData"
+      ${startDateEndDate}
+  )
+  SELECT
+    *
+  FROM
+    "rankedData"
+  WHERE
+      row_num = 1
 `, {
     replacements: {
       BPS: bps,
       CS: cs,
       FDMR: fdmr
     },
-    raw: true
-  })
-  console.log(reportDataIdsResult)
-  const reportDataIds = reportDataIdsResult[0].map(result => result.reportDataId)
-
-  console.log(reportDataIds)
-
-  return db.reportData.findAll({
-    where: {
-      reportDataId: {
-        [db.Sequelize.Op.in]: reportDataIds
-      }
-    },
-    raw: true
+    type: db.sequelize.QueryTypes.SELECT
   })
 }
+
 module.exports = {
   getClaimLevelReportData
 }
