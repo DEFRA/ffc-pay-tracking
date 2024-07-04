2d8669d886fafd5fb346c0674186a2f4ed8ef176
Sam Plackett <samuel.plackett@eviden.com>
2024-07-04 16:31:17 +0100

    refactor function

--- app/payment/update.js
+++ app/payment/update.js
@@ -7,44 +7,57 @@ const { getWhereFilter } = require('../get-where-filter')
 
 const updatePayment = async (event) => {
   const transaction = await db.sequelize.transaction()
+
   try {
     const dbData = await createData(event, transaction)
     const existingData = await getExistingDataFull(event.data, transaction)
+
     if (existingData) {
-      if (isNewSplitInvoiceNumber(event, existingData)) {
-        await createDBFromExisting(dbData, existingData, transaction)
-        const where = getWhereFilter(event)
-        delete where.invoiceNumber
-        if (Object.values(where).every(value => value !== null && value !== undefined)) {
-          const splitEntries = await db.reportData.findAll({
-            where,
-            transaction
-          })
-          if (splitEntries.length > 2 && event.data.originalInvoiceNumber) {
-            where.invoiceNumber = event.data.originalInvoiceNumber
-            await db.reportData.destroy({
-              where,
-              transaction
-            })
-          }
-        }
-      } else {
-        const where = getWhereFilter(event)
-        if (Object.values(where).every(value => value !== null && value !== undefined)) {
-          await db.reportData.update({ ...dbData }, {
-            where,
-            transaction
-          })
-        }
-      }
-      await transaction.commit()
+      await handleExistingData(event, dbData, existingData, transaction)
     } else {
-      await db.reportData.create({ ...dbData }, { transaction })
-      await transaction.commit()
+      await handleNewData(event, dbData, transaction)
     }
+
+    await transaction.commit()
   } catch (error) {
     await transaction.rollback()
-    throw (error)
+    throw error
+  }
+}
+
+const handleExistingData = async (event, dbData, existingData, transaction) => {
+  if (isNewSplitInvoiceNumber(event, existingData)) {
+    await handleSplitInvoiceNumber(event, dbData, existingData, transaction)
+  } else {
+    await handleUpdateExistingData(event, dbData, transaction)
+  }
+}
+
+const handleNewData = async (event, dbData, transaction) => {
+  await db.reportData.create({ ...dbData }, { transaction })
+}
+
+const handleSplitInvoiceNumber = async (event, dbData, existingData, transaction) => {
+  const where = getWhereFilter(event)
+  delete where.invoiceNumber
+
+  if (Object.values(where).every(value => value !== null && value !== undefined)) {
+    const splitEntries = await db.reportData.findAll({ where, transaction })
+
+    if (splitEntries.length > 2 && event.data.originalInvoiceNumber) {
+      where.invoiceNumber = event.data.originalInvoiceNumber
+      await db.reportData.destroy({ where, transaction })
+    }
+  }
+
+  await createDBFromExisting(dbData, existingData, transaction)
+}
+
+const handleUpdateExistingData = async (event, dbData, transaction) => {
+  const where = getWhereFilter(event)
+
+  if (Object.values(where).every(value => value !== null && value !== undefined)) {
+    await db.reportData.update({ ...dbData }, { where, transaction })
   }
 }
 
