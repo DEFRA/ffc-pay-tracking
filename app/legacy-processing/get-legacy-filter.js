94e834d49818191bf5e8532f818012f6f98070c5
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2024-08-01 14:13:13 +0100

    fix deletion issue with migrated data (#22)

--- app/legacy-processing/get-legacy-filter.js
+++ app/legacy-processing/get-legacy-filter.js
@@ -1,22 +1,31 @@
 const { BPS, FDMR, CS } = require('../constants/schemes')
+const db = require('../data')
 
 const getLegacyFilter = (data, schemeId) => {
+  const defaultFilter = {
+    reportDataId: {
+      [db.Sequelize.Op.ne]: data.reportDataId
+    }
+  }
   switch (schemeId) {
     case BPS:
     case FDMR:
       return {
+        ...defaultFilter,
         sourceSystem: data.sourceSystem,
         frn: data.frn,
         marketingYear: data.marketingYear
       }
     case CS:
       return {
+        ...defaultFilter,
         sourceSystem: data.sourceSystem,
         frn: data.frn,
         claimNumber: data.claimNumber
       }
     default:
       return {
+        ...defaultFilter,
         sourceSystem: data.sourceSystem,
         frn: data.frn,
         marketingYear: data.marketingYear,
--- app/legacy-processing/process-legacy-payments.js
+++ app/legacy-processing/process-legacy-payments.js
@@ -9,7 +9,19 @@ const processLegacyPayments = async () => {
     const paymentRequestsBatch = response.payload.paymentRequestsBatch
     for (const paymentRequest of paymentRequestsBatch) {
       console.log('Processing received payment request:', util.inspect(paymentRequest, false, null, true))
-      await processLegacyPaymentRequest(paymentRequest)
+      // in some cases there could be two or more completed payment requests related to a payment request.
+      // if this is the case, we want to process these individually as copies of the same PR.
+      if (paymentRequest.completedPaymentRequests && paymentRequest.completedPaymentRequests.length > 0) {
+        for (const completedRequest of paymentRequest.completedPaymentRequests) {
+          const newPaymentRequest = {
+            ...paymentRequest,
+            completedPaymentRequests: [completedRequest]
+          }
+          await processLegacyPaymentRequest(newPaymentRequest)
+        }
+      } else {
+        await processLegacyPaymentRequest(paymentRequest)
+      }
     }
   } catch (error) {
     console.error('Failed to fetch legacy payment request data:', error)
