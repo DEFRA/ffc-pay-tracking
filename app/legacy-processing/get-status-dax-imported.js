8e97b7d1ec2d8321d0238d5bdaeebf45e063f721
Sam Plackett <samuel.plackett@eviden.com>
2025-05-08 14:25:35 +0100

    add daxImported null

--- app/legacy-processing/get-status-dax-imported.js
+++ app/legacy-processing/get-status-dax-imported.js
@@ -8,10 +8,13 @@ const getStatusDaxImported = (paymentRequest) => {
     fields.daxImported = 'Y'
   } else if (paymentRequest.completedPaymentRequests?.[0]) {
     fields.status = 'Submitted to payment ledger'
+    fields.daxImported = null
   } else if (paymentRequest.debtType) {
     fields.status = 'Waiting for ledger assignment'
+    fields.daxImported = null
   } else {
     fields.status = 'Waiting for debt data'
+    fields.daxImported = null
   }
   return fields
 }
