f7e192484c8fffd50576e3f68175ce621cefd24b
Sam Plackett <samuel.plackett@eviden.com>
2025-10-15 15:37:25 +0100

    trim error length as required

--- test/unit/app/warning/create-data.test.js
+++ test/unit/app/warning/create-data.test.js
@@ -26,3 +26,22 @@ test('createData function returns correct data', () => {
     daxError: mockDAXError
   })
 })
+
+test('should trim the lengths of phError and daxError to no more than 255 chars', () => {
+  const mockEvent = { time: new Date() }
+  const mockStatus = 'warning'
+  const mockPHError = 'ph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph error'
+  const mockDAXError = 'dax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax error'
+
+  getWarningStatus.mockReturnValue(mockStatus)
+  getPHError.mockReturnValue(mockPHError)
+  getDAXError.mockReturnValue(mockDAXError)
+
+  const result = createData(mockEvent)
+
+  expect(getWarningStatus).toHaveBeenCalledWith(mockEvent)
+  expect(getPHError).toHaveBeenCalledWith(mockEvent)
+  expect(getDAXError).toHaveBeenCalledWith(mockEvent)
+  expect(result.phError).toEqual(mockPHError.substring(0, 255))
+  expect(result.daxError).toEqual(mockDAXError.substring(0, 255))
+})
