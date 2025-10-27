898d5016f4e41c9f52df237ae205230d24e53cc5
Sam Plackett <samuel.plackett@eviden.com>
2025-10-27 11:54:08 +0000

    add tests

--- test/unit/app/warning/create-data.test.js
+++ test/unit/app/warning/create-data.test.js
@@ -30,8 +30,8 @@ test('createData function returns correct data', () => {
 test('should trim the lengths of phError and daxError to no more than 255 chars', () => {
   const mockEvent = { time: new Date() }
   const mockStatus = 'warning'
-  const mockPHError = 'ph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph errorph error'
-  const mockDAXError = 'dax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax errordax error'
+  const mockPHError = 'ph error'.repeat(40)
+  const mockDAXError = 'dax error'.repeat(40)
 
   getWarningStatus.mockReturnValue(mockStatus)
   getPHError.mockReturnValue(mockPHError)
@@ -44,4 +44,60 @@ test('should trim the lengths of phError and daxError to no more than 255 chars'
   expect(getDAXError).toHaveBeenCalledWith(mockEvent)
   expect(result.phError).toEqual(mockPHError.substring(0, 255))
   expect(result.daxError).toEqual(mockDAXError.substring(0, 255))
+})
+
+test('should not include phError or daxError when both are null', () => {
+  const mockEvent = { time: new Date() }
+  const mockStatus = 'ok'
+
+  getWarningStatus.mockReturnValue(mockStatus)
+  getPHError.mockReturnValue(null)
+  getDAXError.mockReturnValue(null)
+
+  const result = createData(mockEvent)
+
+  expect(result).toEqual({
+    status: mockStatus,
+    lastUpdated: moment(mockEvent.time).format()
+  })
+  expect(result).not.toHaveProperty('phError')
+  expect(result).not.toHaveProperty('daxError')
+})
+
+test('should not include phError if getPHError returns undefined', () => {
+  const mockEvent = { time: new Date() }
+  const mockStatus = 'warning'
+  const mockDAXError = 'some dax error'
+
+  getWarningStatus.mockReturnValue(mockStatus)
+  getPHError.mockReturnValue(undefined)
+  getDAXError.mockReturnValue(mockDAXError)
+
+  const result = createData(mockEvent)
+
+  expect(result).toEqual({
+    status: mockStatus,
+    lastUpdated: moment(mockEvent.time).format(),
+    daxError: mockDAXError
+  })
+  expect(result).not.toHaveProperty('phError')
+})
+
+test('should not include daxError if getDAXError returns undefined', () => {
+  const mockEvent = { time: new Date() }
+  const mockStatus = 'warning'
+  const mockPHError = 'some ph error'
+
+  getWarningStatus.mockReturnValue(mockStatus)
+  getPHError.mockReturnValue(mockPHError)
+  getDAXError.mockReturnValue(undefined)
+
+  const result = createData(mockEvent)
+
+  expect(result).toEqual({
+    status: mockStatus,
+    lastUpdated: moment(mockEvent.time).format(),
+    phError: mockPHError
+  })
+  expect(result).not.toHaveProperty('daxError')
 })
