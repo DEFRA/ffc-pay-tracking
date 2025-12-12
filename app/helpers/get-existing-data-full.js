0209dfe9168949ccd785f3ad98bbcdeb9cfc2bba
Sam Plackett <samuel.plackett@eviden.com>
2025-12-12 17:34:20 +0000

    fix issue with blank values and doubled up rows

--- app/helpers/get-existing-data-full.js
+++ app/helpers/get-existing-data-full.js
@@ -2,7 +2,7 @@ const db = require('../data')
 const { getDataFilter } = require('./get-data-filter')
 
 const getExistingDataFull = async (data, transaction) => {
-  if (!data.paymentRequestNumber) {
+  if (!data.paymentRequestNumber && data.paymentRequestNumber !== 0) {
     return null
   }
   const where = getDataFilter(data)
